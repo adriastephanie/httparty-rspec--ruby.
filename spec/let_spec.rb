9c91c87d3b215a603366a1e5c05d6452a7add44e
“Adria” <adriastephanie@hotmail.com>
2021-09-21 22:49:21 -0300

    feat: teste usando let com outros let

--- spec/let_spec.rb
+++ spec/let_spec.rb
@@ -2,6 +2,7 @@ describe 'Somar' do
     context 'somar usando let'
         let(:resultado_teste) {3 + 3}
         let(:resultado_exemplo) {6 + 4}
+        let(:result) {resultado_teste + resultado_exemplo}
 
     it 'soma usando o let no teste' do
         puts resultado_teste
@@ -14,5 +15,9 @@ describe 'Somar' do
 
     it 'soma usando var let' do 
         expect(resultado_exemplo).to eq 10
+    end
+
+    it 'soma usando as duas variaveis  let' do 
+        expect(result).to eq 16
     end
 end
